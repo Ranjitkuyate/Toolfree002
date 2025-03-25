fbdb6d0445f1bd4029519cbc3f4e7ad7d3ce5711
Ranjitkuyate <156173622+Ranjitkuyate@users.noreply.github.com>
2025-03-25 15:55:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,40 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['toolsfree.online'],
-  },
-  async headers() {
-    return [
-      {
-        // This is needed for Propeller Ads verification file
-        source: '/sw.js',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=0, must-revalidate',
-          },
-          {
-            key: 'Service-Worker-Allowed',
-            value: '/',
-          },
-          {
-            key: 'Content-Type',
-            value: 'application/javascript',
-          },
-        ],
-      },
-    ];
-  },
-  // Configure service worker registration
-  async rewrites() {
-    return [
-      {
-        source: '/sw.js',
-        destination: '/sw.js',
-      },
-    ];
+  experimental: {
+    appDir: true,
   },
 };
 
