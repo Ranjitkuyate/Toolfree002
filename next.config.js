7999600304348fcef28e511ce40bb5336d90447a
Ranjitkuyate <156173622+Ranjitkuyate@users.noreply.github.com>
2025-03-25 22:43:10 +0530

    Update and rename next.config.js to next.config.mjs

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    serverActions: {}, // Fix: Must be an object (not boolean)
-  },
-};
-
-module.exports = nextConfig;
---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+import { NextConfig } from 'next';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default nextConfig;
