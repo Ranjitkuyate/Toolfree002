6a24d18609db780a555e8f33ecbd6470e3c82c71
Ranjitkuyate <156173622+Ranjitkuyate@users.noreply.github.com>
2025-03-26 20:28:00 +0530

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,44 +1,38 @@
 import React from 'react';
 import Head from 'next/head';
-import Header from './Header';
-import Footer from './Footer';
 import styled from 'styled-components';
+import Header from './layout/Header';
+import Footer from './layout/Footer';
 
 interface LayoutProps {
   children: React.ReactNode;
+  title?: string;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<LayoutProps> = ({ 
+  children, 
+  title = 'ToolsFree Online - Free Online Tools' 
+}) => {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <title>{title}</title>
+        <meta name="description" content="Free online tools to make your life easier" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       
-      <MainContainer>
-        <Header />
-        <MainContent>
-          {children}
-        </MainContent>
-        <Footer />
-      </MainContainer>
+      <Header />
+      
+      <Main>{children}</Main>
+      
+      <Footer />
     </>
   );
 };
 
-const MainContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-`;
-
-const MainContent = styled.main`
-  flex: 1;
-  background-color: var(--background-color);
+const Main = styled.main`
+  min-height: calc(100vh - 200px);
 `;
 
 export default Layout;
