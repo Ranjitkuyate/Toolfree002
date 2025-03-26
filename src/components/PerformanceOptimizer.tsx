538a06a7ae030be54b8da2450fb768fa43c535ec
Ranjitkuyate <156173622+Ranjitkuyate@users.noreply.github.com>
2025-03-26 08:43:09 +0530

    Update PerformanceOptimizer.tsx

--- src/components/PerformanceOptimizer.tsx
+++ src/components/PerformanceOptimizer.tsx
@@ -4,8 +4,9 @@ const PerformanceOptimizer: React.FC = () => {
   return (
     <div>
       <h2>Performance Optimizer</h2>
-      <p>Don&apos;t forget to minify your JavaScript and CSS.</p>
-      <p>Use lazy loading for &quot;better performance&quot;.</p>
+      <p>Don&apos;t forget to minify your JavaScript and CSS for &quot;better performance&quot;.</p>
+      <p>Lazy loading helps in optimizing page speed &amp; reducing load times.</p>
+      <p>Use server-side rendering (SSR) in Next.js for SEO benefits.</p>
     </div>
   );
 };
