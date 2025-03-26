b274c507d0735e933f7f625e73d4275b02c7e929
Ranjitkuyate <156173622+Ranjitkuyate@users.noreply.github.com>
2025-03-26 07:24:08 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,7 @@
-{
-  "root": true,
-  "extends": [
-    "next/core-web-vitals",
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended"
-  ],
-  "plugins": ["@typescript-eslint", "react"],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
+module.exports = {
+  extends: ["next", "eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: 2020,
   },
-  "env": {
-    "browser": true,
-    "node": true,
-    "es6": true
-  },
-  "rules": {
-    "react/react-in-jsx-scope": "off",
-    "no-unused-vars": "warn",
-    "semi": ["error", "always"],
-    "quotes": ["error", "double"],
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/explicit-function-return-type": "off"
-  }
-}
+  rules: {},
+};
