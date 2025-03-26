7267590aeea6ced19bf6823278c91cffbd17c87d
Ranjitkuyate <156173622+Ranjitkuyate@users.noreply.github.com>
2025-03-26 08:15:26 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ["next", "next/core-web-vitals", "eslint:recommended", "plugin:react/recommended"],
+  extends: ["next/core-web-vitals"],
   parserOptions: {
     ecmaVersion: 2021,
     sourceType: "module",
