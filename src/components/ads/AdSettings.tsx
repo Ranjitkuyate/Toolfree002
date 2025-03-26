e6c89f5cfad926d25b5b558de6f00e7835dfc555
Ranjitkuyate <156173622+Ranjitkuyate@users.noreply.github.com>
2025-03-26 09:01:20 +0530

    Update AdSettings.tsx

--- src/components/ads/AdSettings.tsx
+++ src/components/ads/AdSettings.tsx
@@ -1,12 +1,22 @@
-interface AdSettingsProps {
-  adsEnabled: boolean;
-  toggleAdsEnabled: () => void;
-}
+import React from "react";
+import PropTypes from "prop-types";
 
-const AdSettings: React.FC<AdSettingsProps> = ({ adsEnabled, toggleAdsEnabled }) => (
-  <button onClick={toggleAdsEnabled}>
-    {adsEnabled ? "Disable Ads" : "Enable Ads"}
-  </button>
-);
+const AdSettings = ({ adsEnabled, toggleAdsEnabled }) => {
+  return (
+    <div>
+      <h2>Ad Settings</h2>
+      <label>
+        <input type="checkbox" checked={adsEnabled} onChange={toggleAdsEnabled} />
+        Enable Ads
+      </label>
+    </div>
+  );
+};
+
+// Add PropTypes validation
+AdSettings.propTypes = {
+  adsEnabled: PropTypes.bool.isRequired,
+  toggleAdsEnabled: PropTypes.func.isRequired,
+};
 
 export default AdSettings;
