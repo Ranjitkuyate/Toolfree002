3d80367321f3bc7ed610b2b574bf9c8e9a636f05
Ranjitkuyate <156173622+Ranjitkuyate@users.noreply.github.com>
2025-03-26 08:48:29 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,41 @@
-{
-  "root": true,
-  "extends": [
-    "next/core-web-vitals",
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended"
-  ],
-  "plugins": ["@typescript-eslint", "react"],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
-  },
-  "env": {
-    "browser": true,
-    "node": true,
-    "es6": true
-  },
-  "rules": {
-    "react/react-in-jsx-scope": "off",
-    "no-unused-vars": "warn",
-    "semi": ["error", "always"],
-    "quotes": ["error", "double"],
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/explicit-function-return-type": "off"
-  }
-}
+// AnalyticsIntegration.tsx
+import React from 'react';
+
+const AnalyticsIntegration = () => {
+  return (
+    <div>
+      <h1>SEO Analytics &amp; Performance</h1>
+      <p>Use Google Analytics to track visitor interactions.</p>
+    </div>
+  );
+};
+
+export default AnalyticsIntegration;
+
+// PerformanceOptimizer.tsx
+import React from 'react';
+
+const PerformanceOptimizer = () => {
+  return (
+    <div>
+      <h1>Performance Optimization</h1>
+      <p>Optimize your site&apos;s loading speed using best practices.</p>
+    </div>
+  );
+};
+
+export default PerformanceOptimizer;
+
+// RobotsTxtGenerator.tsx
+import React from 'react';
+
+const RobotsTxtGenerator = () => {
+  return (
+    <div>
+      <h1>Robots.txt Generator</h1>
+      <p>Configure your robots.txt file to manage search engine indexing.</p>
+    </div>
+  );
+};
+
+export default RobotsTxtGenerator;
