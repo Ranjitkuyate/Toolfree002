d76ace03935db2cb7698429b7d7033bbe3407c4d
Ranjitkuyate <156173622+Ranjitkuyate@users.noreply.github.com>
2025-03-27 09:02:01 +0530

    Update image-compressor.tsx

--- src/pages/tools/image/image-compressor.tsx
+++ src/pages/tools/image/image-compressor.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Layout from '@/components/Layout';
+import Layout from '@/components/layout/Layout';
 import SEO from '@/components/SEO';
 import ImageCompressor from '@/components/tools/image/ImageCompressor';
 import AdBanner from '@/components/AdBanner';
