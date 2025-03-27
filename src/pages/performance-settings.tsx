d79e16b2059ff1376218cef7551bdf29f7fbfc97
Ranjitkuyate <156173622+Ranjitkuyate@users.noreply.github.com>
2025-03-27 09:00:21 +0530

    Update performance-settings.tsx

--- src/pages/performance-settings.tsx
+++ src/pages/performance-settings.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Layout from '@/components/Layout';
+import Layout from '@/components/layout/Layout';
 import SectionTitle from '@/components/SectionTitle';
 import PerformanceOptimizer from '@/components/PerformanceOptimizer';
 import AdBanner from '@/components/AdBanner';
